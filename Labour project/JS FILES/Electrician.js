7ecef4c04755125e460252cd28dade4899d8c40a
Shubham Gupta <sg4658749@gmail.com>
2025-08-01 22:53:13 -0800

    Add files via upload

---
+++ Labour project/JS FILES/Electrician.js
@@ -0,0 +1,243 @@
+const labourData = [
+  {
+    name: "Raj Kumar",
+    category: "Electrician",
+    area: "Karol Bagh",
+    location: "Delhi",
+    contact: "+91 9876543210",
+    image: "https://i.ibb.co/Ttvv1cD/electrician.jpg"
+  },
+  {
+    name: "Amit Singh",
+    category: "Plumber",
+    area: "Lajpat Nagar",
+    location: "Delhi",
+    contact: "+91 9123456780",
+    image: "https://i.ibb.co/g9S6S1V/plumber.jpg"
+  },
+  {
+    name: "Suman Devi",
+    category: "Painter",
+    area: "Bandra",
+    location: "Mumbai",
+    contact: "+91 9988776655",
+    image: "https://i.ibb.co/MZg8BZ0/painter.jpg"
+  },
+  {
+    name: "Ravi Sharma",
+    category: "Carpenter",
+    area: "Vaishali Nagar",
+    location: "Jaipur",
+    contact: "+91 9981234567",
+    image: "https://i.ibb.co/NZw59gn/carpenter.jpg"
+  },
+  {
+    name: "Sunita Yadav",
+    category: "Electrician",
+    area: "Hazratganj",
+    location: "Lucknow",
+    contact: "+91 8899776655",
+    image: "https://i.ibb.co/k0dMmt9/woman-electrician.jpg"
+  },
+  {
+    name: "Raj Kumar",
+    category: "Electrician",
+    area: "Karol Bagh",
+    location: "Delhi",
+    contact: "+91 9876543210",
+    image: "https://i.ibb.co/Ttvv1cD/electrician.jpg"
+  },
+  {
+    name: "Amit Singh",
+    category: "Plumber",
+    area: "Lajpat Nagar",
+    location: "Delhi",
+    contact: "+91 9123456780",
+    image: "https://i.ibb.co/g9S6S1V/plumber.jpg"
+  },
+  {
+    name: "Suman Devi",
+    category: "Painter",
+    area: "Bandra",
+    location: "Mumbai",
+    contact: "+91 9988776655",
+    image: "https://i.ibb.co/MZg8BZ0/painter.jpg"
+  },
+  {
+    name: "Ravi Sharma",
+    category: "Carpenter",
+    area: "Vaishali Nagar",
+    location: "Jaipur",
+    contact: "+91 9981234567",
+    image: "https://i.ibb.co/NZw59gn/carpenter.jpg"
+  },
+  {
+    name: "Sunita Yadav",
+    category: "Electrician",
+    area: "Hazratganj",
+    location: "Lucknow",
+    contact: "+91 8899776655",
+    image: "https://i.ibb.co/k0dMmt9/woman-electrician.jpg"
+  },
+  {
+    name: "Raj Kumar",
+    category: "Electrician",
+    area: "Karol Bagh",
+    location: "Delhi",
+    contact: "+91 9876543210",
+    image: "https://i.ibb.co/Ttvv1cD/electrician.jpg"
+  },
+  {
+    name: "Amit Singh",
+    category: "Plumber",
+    area: "Lajpat Nagar",
+    location: "Delhi",
+    contact: "+91 9123456780",
+    image: "https://i.ibb.co/g9S6S1V/plumber.jpg"
+  },
+  {
+    name: "Suman Devi",
+    category: "Painter",
+    area: "Bandra",
+    location: "Mumbai",
+    contact: "+91 9988776655",
+    image: "https://i.ibb.co/MZg8BZ0/painter.jpg"
+  },
+  {
+    name: "Ravi Sharma",
+    category: "Carpenter",
+    area: "Vaishali Nagar",
+    location: "Jaipur",
+    contact: "+91 9981234567",
+    image: "https://i.ibb.co/NZw59gn/carpenter.jpg"
+  },
+  {
+    name: "Sunita Yadav",
+    category: "Electrician",
+    area: "Hazratganj",
+    location: "Lucknow",
+    contact: "+91 8899776655",
+    image: "https://i.ibb.co/k0dMmt9/woman-electrician.jpg"
+  },
+  {
+    name: "Raj Kumar",
+    category: "Electrician",
+    area: "Karol Bagh",
+    location: "Delhi",
+    contact: "+91 9876543210",
+    image: "https://i.ibb.co/Ttvv1cD/electrician.jpg"
+  },
+  {
+    name: "Amit Singh",
+    category: "Plumber",
+    area: "Lajpat Nagar",
+    location: "Delhi",
+    contact: "+91 9123456780",
+    image: "https://i.ibb.co/g9S6S1V/plumber.jpg"
+  },
+  {
+    name: "Suman Devi",
+    category: "Painter",
+    area: "Bandra",
+    location: "Mumbai",
+    contact: "+91 9988776655",
+    image: "https://i.ibb.co/MZg8BZ0/painter.jpg"
+  },
+  {
+    name: "Ravi Sharma",
+    category: "Carpenter",
+    area: "Vaishali Nagar",
+    location: "Jaipur",
+    contact: "+91 9981234567",
+    image: "https://i.ibb.co/NZw59gn/carpenter.jpg"
+  },
+  {
+    name: "Sunita Yadav",
+    category: "Electrician",
+    area: "Hazratganj",
+    location: "Lucknow",
+    contact: "+91 8899776655",
+    image: "https://i.ibb.co/k0dMmt9/woman-electrician.jpg"
+  },
+  {
+    name: "Raj Kumar",
+    category: "Electrician",
+    area: "Karol Bagh",
+    location: "Delhi",
+    contact: "+91 9876543210",
+    image: "https://i.ibb.co/Ttvv1cD/electrician.jpg"
+  },
+  {
+    name: "Amit Singh",
+    category: "Plumber",
+    area: "Lajpat Nagar",
+    location: "Delhi",
+    contact: "+91 9123456780",
+    image: "https://i.ibb.co/g9S6S1V/plumber.jpg"
+  },
+  {
+    name: "Suman Devi",
+    category: "Painter",
+    area: "Bandra",
+    location: "Mumbai",
+    contact: "+91 9988776655",
+    image: "https://i.ibb.co/MZg8BZ0/painter.jpg"
+  },
+  {
+    name: "Ravi Sharma",
+    category: "Carpenter",
+    area: "Vaishali Nagar",
+    location: "Jaipur",
+    contact: "+91 9981234567",
+    image: "https://i.ibb.co/NZw59gn/carpenter.jpg"
+  },
+  {
+    name: "Sunita Yadav",
+    category: "Electrician",
+    area: "Hazratganj",
+    location: "Lucknow",
+    contact: "+91 8899776655",
+    image: "https://i.ibb.co/k0dMmt9/woman-electrician.jpg"
+  },
+  {
+    name: "Raj Kumar",
+    category: "Electrician",
+    area: "Karol Bagh",
+    location: "Delhi",
+    contact: "+91 9876543210",
+    image: "https://i.ibb.co/Ttvv1cD/electrician.jpg"
+  },
+  {
+    name: "Amit Singh",
+    category: "Plumber",
+    area: "Lajpat Nagar",
+    location: "Delhi",
+    contact: "+91 9123456780",
+    image: "https://i.ibb.co/g9S6S1V/plumber.jpg"
+  },
+  {
+    name: "Suman Devi",
+    category: "Painter",
+    area: "Bandra",
+    location: "Mumbai",
+    contact: "+91 9988776655",
+    image: "https://i.ibb.co/MZg8BZ0/painter.jpg"
+  },
+  {
+    name: "Ravi Sharma",
+    category: "Carpenter",
+    area: "Vaishali Nagar",
+    location: "Jaipur",
+    contact: "+91 9981234567",
+    image: "https://i.ibb.co/NZw59gn/carpenter.jpg"
+  },
+  {
+    name: "Sunita Yadav",
+    category: "Electrician",
+    area: "Hazratganj",
+    location: "Lucknow",
+    contact: "+91 8899776655",
+    image: "https://i.ibb.co/k0dMmt9/woman-electrician.jpg"
+  }
+  
+];
